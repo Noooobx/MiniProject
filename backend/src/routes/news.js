7da67143a7cc96f8acc2823d2f87133bb0cd203c
SilentCodeSage <nandunandakishor345@gmail.com>
2025-03-22 11:35:28 +0530

    fix env

--- backend/src/routes/news.js
+++ backend/src/routes/news.js
@@ -7,14 +7,11 @@ newsRouter.get("/view", async (req, res) => {
   try {
     const apiKey = process.env.NEWS_API_KEY || "978b530dc1c042268598aab3b901510e";
     console.log(apiKey)
-    const response = await axios.get(`https://newsapi.org/v2/everything`, {
-      params: {
-        q: "agriculture OR farming OR crops",
-        language: "en",
-        sortBy: "publishedAt",
-        apiKey: apiKey,
-      },
-    });
+    const response = await axios.get("https://newsapi.org/v2/everything", {
+        params: { q: "agriculture OR farming OR crops", language: "en", sortBy: "publishedAt", apiKey },
+        headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" }
+      });
+      
 
     res.json(response.data); // Send the fetched news as JSON
   } catch (error) {
