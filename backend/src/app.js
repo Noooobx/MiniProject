c493eb0d23c3df20643b2fec39b3739f634a026c
SilentCodeSage <nandunandakishor345@gmail.com>
2025-03-23 11:25:30 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,10 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express(); 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://mini-project-psi-navy.vercel.app"],
+  credentials: true 
+}));
 app.use(express.json());  
 app.use(cookieParser());
 
